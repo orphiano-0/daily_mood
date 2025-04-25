757a5f94385f3c4ba6fcd74f8fc4cc93b1bd31d9
orphiano-0 <gab.orphiano@gmail.com>
2025-04-25 18:00:25 +0800

    finalization, we're finished

--- lib/shared/theme/light_mode.dart
+++ lib/shared/theme/light_mode.dart
@@ -2,50 +2,12 @@ import 'package:flutter/material.dart';
 
 final lightTheme = ThemeData(
   primaryColor: Colors.white,
-  scaffoldBackgroundColor: Colors.white,
-  appBarTheme: const AppBarTheme(
-    backgroundColor: Colors.blue,
-    foregroundColor: Colors.white,
-    elevation: 0,
-  ),
-  cardTheme: CardTheme(
-    color: Colors.white,
-    elevation: 2,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(12),
-    ),
-  ),
-  elevatedButtonTheme: ElevatedButtonThemeData(
-    style: ElevatedButton.styleFrom(
-      backgroundColor: Colors.blue,
-      foregroundColor: Colors.white,
-      elevation: 0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-      ),
-    ),
-  ),
-  textButtonTheme: TextButtonThemeData(
-    style: TextButton.styleFrom(
-      foregroundColor: Colors.blue,
-    ),
-  ),
-  sliderTheme: const SliderThemeData(
-    activeTrackColor: Colors.blue,
-    thumbColor: Colors.blue,
-  ),
   colorScheme: ColorScheme.light(
     primary: Colors.white,
     secondary: Colors.blueAccent,
     surface: Colors.white,
     error: Colors.red,
     inversePrimary: Colors.black
-  ),
-  dialogTheme: DialogTheme(
-    backgroundColor: Colors.white,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(16),
-    ),
   ),
 );
 
